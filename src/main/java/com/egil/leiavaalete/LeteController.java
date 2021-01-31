7a4c1477e9f3d91942ca22b647596864c04d4971
Egil Rian <egil.rian@systek.no>
2021-01-30 21:06:47 -0800

    added nextdoor and finn. trouble with logged in user for nd. Need to encode multiple params with %20. Getting 404 on finn

--- src/main/java/com/egil/leiavaalete/LeteController.java
+++ src/main/java/com/egil/leiavaalete/LeteController.java
@@ -2,53 +2,45 @@ package com.egil.leiavaalete;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.net.URLEncoder;
-import java.util.HashMap;
-import java.util.Map;
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class LeteController {
 
-    public static String getParamsString(Map<String, String> params) throws UnsupportedEncodingException {
-        StringBuilder result = new StringBuilder();
+    public static final String URL_CRAIGSLIST = "https://seattle.craigslist.org/d/for-sale/search/sss?query=";
+    public static final String URL_NEXTDOOR = "https://nextdoor.com/for_sale_and_free/?init_source=more_menu&query=";
+    public static final String URL_FINN = "https://www.finn.no/bap/forsale/search.html?q=";
+    public static final String URL_GOOGLE = "http://google.com";
 
-        for (Map.Entry<String, String> entry : params.entrySet()) {
-            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
-            result.append("=");
-            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
-            result.append("&");
-        }
-
-        String resultString = result.toString();
-        return resultString.length() > 0
-                ? resultString.substring(0, resultString.length() - 1)
-                : resultString;
-    }
-
-    @RequestMapping(value = "/egil")
+    /**
+     * @param query example: "nikkor+1.4"
+     * @return
+     */
+    @RequestMapping(value = "/lete")
     @GetMapping
-    public String egilEndpoint() {
+    public String egilEndpoint(@RequestParam String site, @RequestParam String query) {
         StringBuffer content = new StringBuffer();
         try {
-            String craigslistUrl = "https://seattle.craigslist.org";
-            URL url = new URL(craigslistUrl + "/search/sss");
+            String siteUrl = getSiteUrl(site);
+            URL url = new URL(siteUrl + encodeQuery(query));
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
             connection.setRequestMethod(GET.name());
-            Map<String, String> parameters = new HashMap<>();
-            parameters.put("query", "nikkor+1.4");
             connection.setDoOutput(true);
             DataOutputStream out = new DataOutputStream(connection.getOutputStream());
-            out.writeBytes(getParamsString(parameters));
             out.flush();
             out.close();
             int responseCode = connection.getResponseCode();
+            System.out.println("responseCode = " + responseCode);
             BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
             String inputLine;
             content = new StringBuffer();
@@ -61,7 +53,23 @@ public class LeteController {
             e.printStackTrace();
         }
 
-
         return content.toString();
+    }
+
+    private String encodeQuery(String query) {
+        return query.replaceAll("\\+", "%20");
+    }
+
+    private String getSiteUrl(String site) {
+        switch (site) {
+            case "cl":
+                return URL_CRAIGSLIST;
+            case "nd":
+                return URL_NEXTDOOR;
+            case "fn":
+                return URL_FINN;
+            default:
+                return URL_GOOGLE;
+        }
     }
 }
