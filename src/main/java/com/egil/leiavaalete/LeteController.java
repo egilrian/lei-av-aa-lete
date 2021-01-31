600d4a8e2773a75a7fe759930d0e87cb59c2b7f9
Egil Rian <egil.rian@systek.no>
2021-01-30 17:45:11 -0800

    added greeting controller, updated pom, added index html

--- src/main/java/com/egil/leiavaalete/LeteController.java
+++ src/main/java/com/egil/leiavaalete/LeteController.java
@@ -1,16 +1,13 @@
 package com.egil.leiavaalete;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LeteController {
-
-    @RequestMapping("/")
-    public String index() {
-        return "Greetings from Spring Boot!";
-    }
 
     @RequestMapping(value="/egil")
     @GetMapping
