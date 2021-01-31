3ee7d4ae2fbcdcffa7d7104b540552d4c04d2a63
Egil Rian <egil.rian@systek.no>
2021-01-30 18:48:23 -0800

    added greeting controller, updated pom, added index html

--- src/main/java/com/egil/leiavaalete/LeteController.java
+++ src/main/java/com/egil/leiavaalete/LeteController.java
@@ -1,17 +1,67 @@
 package com.egil.leiavaalete;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.net.URLEncoder;
+import java.util.HashMap;
+import java.util.Map;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
 
 @RestController
 public class LeteController {
 
-    @RequestMapping(value="/egil")
+    public static String getParamsString(Map<String, String> params) throws UnsupportedEncodingException {
+        StringBuilder result = new StringBuilder();
+
+        for (Map.Entry<String, String> entry : params.entrySet()) {
+            result.append(URLEncoder.encode(entry.getKey(), "UTF-8"));
+            result.append("=");
+            result.append(URLEncoder.encode(entry.getValue(), "UTF-8"));
+            result.append("&");
+        }
+
+        String resultString = result.toString();
+        return resultString.length() > 0
+                ? resultString.substring(0, resultString.length() - 1)
+                : resultString;
+    }
+
+    @RequestMapping(value = "/egil")
     @GetMapping
-    public String egilEndpoint(){
-        return "egil";
+    public String egilEndpoint() {
+        StringBuffer content = new StringBuffer();
+        try {
+            String craigslistUrl = "https://seattle.craigslist.org";
+            URL url = new URL(craigslistUrl + "/search/sss");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.setRequestMethod(GET.name());
+            Map<String, String> parameters = new HashMap<>();
+            parameters.put("query", "nikkor+1.4");
+            connection.setDoOutput(true);
+            DataOutputStream out = new DataOutputStream(connection.getOutputStream());
+            out.writeBytes(getParamsString(parameters));
+            out.flush();
+            out.close();
+            int responseCode = connection.getResponseCode();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+            String inputLine;
+            content = new StringBuffer();
+            while ((inputLine = reader.readLine()) != null) {
+                content.append(inputLine);
+            }
+            reader.close();
+            connection.disconnect();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        return content.toString();
     }
 }
